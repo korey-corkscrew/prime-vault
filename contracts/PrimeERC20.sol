b4fb4a3638fa43757edb873aa31a2a287b59de81
Korey Corkscrew <77598913+korey-corkscrew@users.noreply.github.com>
2022-10-07 10:43:46 -0500

    update

--- contracts/PrimeERC20.sol
+++ contracts/PrimeERC20.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PrimeERC20 is ERC20 {
+    uint256 MAX_SUPPLY = 1000000 ether;
+
     constructor() ERC20("Prime", "PRIME") {}
 }
